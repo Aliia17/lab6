b450a50fc715f5a0ca04c639c4b6ab87e5dab084
Aliia17 <lianice170899@mail.ru>
2023-02-28 16:49:08 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+//Task 1//
+function printHW () {
+for (let i = 0; i < 3; i++) {
+  console.log("Hello, World!")
+  }
+}
+  
+printHW()
+
+//Task 10//
+
+
+function equality (number1, number2){
+  if (number1 == number2 ) {
+    console.log ("X is equal to Y")
+}
+else {
+    console.log ("X is not equal to Y")
+}
+}
+
+let E = equality(2, 3)
+
+
+//Task 7//
+
+function product (n1, n2, n3) {
+  result = n1 * n2 * n3
+  return result 
+}
+
+let a = 2
+let b = 3
+let c = 4
+
+let d = product(a,b,c)
+console.log(d)
